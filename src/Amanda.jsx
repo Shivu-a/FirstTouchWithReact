1678e52af6bc258ccbc47c13b9b36eeb393330a7
Shivu-a <sebaingame@gmail.com>
2022-07-01 22:57:00 -0300

    Playing with my first component

---
+++ src/Amanda.jsx
@@ -0,0 +1,7 @@
+import "./Amanda.css";
+
+function Amanda() {
+  return <h1>Amanda</h1>;
+}
+
+export default Amanda;
---
+++ src/Cubo.jsx
@@ -0,0 +1,13 @@
+import "./cubo.css";
+
+function Cubo() {
+  return (
+    <div className="cubo">
+      <div className="cubo">
+        <div className="cubo interior"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Cubo;
